cf45fc3d7175658a1173d0594c94e346447eef57
Jules Pabst <julia.pabst@kununu.com>
2025-05-07 11:49:37 +0200

    add menu bar and search bar

--- TourPlanner/src/main/java/org/tourplanner/view/MainController.java
+++ TourPlanner/src/main/java/org/tourplanner/view/MainController.java
@@ -2,13 +2,15 @@ package org.tourplanner.view;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController {
+    public TextField searchField;
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onSearch() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
