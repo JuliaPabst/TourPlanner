d8bf4cf9ef015f4c676b2486e13ab37c3c82befa
SofiaHKG <145010488+SofiaHKG@users.noreply.github.com>
2025-07-02 12:38:12 +0200

    refactor: add logging to TourPlannerApplication

--- TourPlanner/src/main/java/org/tourplanner/TourPlannerApplication.java
+++ TourPlanner/src/main/java/org/tourplanner/TourPlannerApplication.java
@@ -8,42 +8,48 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
 public class TourPlannerApplication extends Application {
+    private static final Logger log = LogManager.getLogger(TourPlannerApplication.class);
     private ConfigurableApplicationContext springContext;
 
     @Override
     public void init() {
+        log.info("Initializing Spring context");
         springContext = new SpringApplicationBuilder(TourPlannerConfig.class).run(getParameters().getRaw().toArray(new String[0]));
+        log.info("Spring context initialized successfully.");
     }
 
     @Override
     public void start(Stage stage) throws IOException {
+        log.info("Starting JavaFX application");
         FXMLLoader fxmlLoader = new FXMLLoader(TourPlannerApplication.class.getResource("main-view.fxml"));
-
         fxmlLoader.setControllerFactory(springContext::getBean);
-
         Parent root = fxmlLoader.load();
         showStage(stage, root);
+        log.info("JavaFX application started");
     }
 
     public static void showStage(Stage stage, Parent root) {
         Scene scene = new Scene(root, 1080, 500);
-        stage.setTitle("TaskPlanner");
+        stage.setTitle("Tour Planner");
         stage.setScene(scene);
         stage.show();
     }
 
     @Override
     public void stop() {
+        log.info("Stopping application: closing Spring context and exiting JavaFX platform.");
         springContext.close();
         Platform.exit();
     }
 
     public static void main(String[] args) {
+        log.info("Launching application");
         launch();
     }
 }
