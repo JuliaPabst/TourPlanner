4d2dbd7387f9288761c7d57e4082e05bbf9eed7c
SofiaHKG <145010488+SofiaHKG@users.noreply.github.com>
2025-07-03 02:21:12 +0200

    inlcude meta table in Tour PDF

--- TourPlanner/src/main/java/org/tourplanner/service/ReportService.java
+++ TourPlanner/src/main/java/org/tourplanner/service/ReportService.java
@@ -3,7 +3,10 @@ package org.tourplanner.service;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.properties.UnitValue;
 import org.springframework.stereotype.Service;
 import org.tourplanner.persistence.entity.Tour;
 
@@ -13,14 +16,13 @@ import java.nio.file.Path;
 @Service
 public class ReportService {
 
-    // TODO: No tour parameters yet, just for testing PDF creation
     public void generateTourReport(Tour tour, Path target) throws IOException {
         try(PdfWriter writer = new PdfWriter(target.toString());
             PdfDocument pdf = new PdfDocument(writer);
             Document doc = new Document(pdf)) {
 
             doc.add(new Paragraph("Tour Report – " + tour.getTourName()));
-            // TODO: logs, tables...
+            addMetaTable(tour, doc);
         }
     }
 
@@ -31,5 +33,27 @@ public class ReportService {
 
             doc.add(new Paragraph("Summary Report – Placeholder"));
         }
+    }
+
+    private void addMetaTable(Tour tour, Document doc) {
+        Table table = new Table(UnitValue.createPercentArray(2))
+                .useAllAvailableWidth();
+
+        table.addCell(new Cell().add(new Paragraph("From")));
+        table.addCell(new Cell().add(new Paragraph(tour.getFrom())));
+
+        table.addCell(new Cell().add(new Paragraph("To")));
+        table.addCell(new Cell().add(new Paragraph(tour.getTo())));
+
+        table.addCell(new Cell().add(new Paragraph("Distance [km]")));
+        table.addCell(new Cell().add(new Paragraph(String.valueOf(tour.getDistance()))));
+
+        table.addCell(new Cell().add(new Paragraph("Est. time [min]")));
+        table.addCell(new Cell().add(new Paragraph(String.valueOf(tour.getEstimatedTime()))));
+
+        table.addCell(new Cell().add(new Paragraph("Transport")));
+        table.addCell(new Cell().add(new Paragraph(tour.getTransportType().getLabel())));
+
+        doc.add(table);
     }
 }
